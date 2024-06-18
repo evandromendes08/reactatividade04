b4d5a3d18c245b144f1ae21fc0cb4c405b9f11fd
Evandro Mendes <evandromende@gmail.com>
2024-06-17 22:57:31 -0300

    Feat: Service

--- src/pages/AddProducts/index.js
+++ src/pages/AddProducts/index.js
@@ -67,12 +67,12 @@ const AddProduct = () => {
   return (
     <section className="my-12 max-w-screen-xl mx-auto px-6">
       <div className="flex flex-col space-y-2">
-        <h1 className="text-2xl text-gray-600">Cadastro de Produtos</h1>
+        <h1 className="text-2xl text-gray-600">Cadastrar Produtos</h1>
       </div>
-      <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10 mt-6">
+      ----<form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10 mt-6">
         <div className="flex flex-col space-y-4">
           <label htmlFor="nome" className="text-gray-500">
-            Nome
+            Nome do Produto
           </label>
           <input
             type="text"
@@ -84,17 +84,17 @@ const AddProduct = () => {
           />
 
           <label htmlFor="descricao" className="text-gray-500">
-            Descrição
+            Descrição do Produto
           </label>
           <textarea
             name="descricao"
             id="descricao"
             cols="30"
             rows="5"
-            onChange={handleChangeValues}
-            className="border border-gray-200 rounded-lg py-3 px-4 w-full focus:outline-none ring-red-200 transition duration-500 focus:ring-4 resize-none"
             required
-          ></textarea>
+            onChange={handleChangeValues}
+            className="border border-gray-200 rounded-lg py-3 px-4 w-full focus:outline-none ring-red-200 transition duration-500 focus:ring-4 resize-none">
+            </textarea>
 
           <label htmlFor="codigoBarra" className="text-gray-500">
             Código de barras
@@ -143,7 +143,9 @@ const AddProduct = () => {
             labelledBy="Select"
           />
           <div className="mt-8">
-            <button className="w-full py-3 bg-primary text-white ring-red-400 focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300">Adicionar</button>
+            <button className="w-full py-3 bg-primary text-white ring-red-400 focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300">
+                Adicionar
+            </button>
           </div>
         </div>
       </form>
